19e981cacbd3068d625c88a6c315d8ec755faa9e
SixArm <sixarm@sixarm.com>
2011-10-06 21:30:08 -0700

    Updates for gem publishing

--- lib/sixarm_ruby_active_record_migration_mock.rb
+++ lib/sixarm_ruby_active_record_migration_mock.rb
@@ -1,66 +1,6 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm.com » Ruby » ActiveRecord migration mock object for testing Rails
-
-Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2009-2011 Joel Parker Henderson
-License:: See LICENSE.txt file
-
-ActiveRecord migration mock object for testing our Ruby On Rails gems.
-
-This is useful when you don't want to connect to the real database.
-
-==Example Migration
-
-  class CreateFoosMigration < ActiveRecordMigrationMock
-
-   def self.up
-     create_table 'foos', :force => true do |t|
-       t.column :abc,  :integer
-       t.column :def,  :string
-     end
-     add_index 'foos', 'abc'
-   end
-
-   def self.down
-     drop_table 'foos'
-   end
-
- end
-
-==Example Usage
-
-  CreateFoosMigration.up
-
-  ActiveRecordMigration.has_table?('foos') => true
-  ActiveRecordMigration.has_table?('bars') => false
-
-  tables = ActiveRecordMigration.tables
-
-  table = ActiveRecordMigration.tables['foo']
-
-  table.has_column?('abc') => true
-  table.has_column?('xyz') => false
-
-  table.has_index?('abc') => true
-  table.has_index?('def') => false
-
-  columns = table.columns
-
-  column = table.columns['abc']
-  column.type => :integer
-
-  column = table.columns['def']
-  column.type => :string
-
-  indexes = table.indexes
-  index = table.indexes['abc']
-
-  index.table_name => 'foos'
-  index.column_name => 'abc'
-
+Please see README.rdoc
 =end
 
 
